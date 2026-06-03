de6552e77bf99d4c48bc243e4fad87f422c1f2ee
AhmadKadafi29 <kadafiahmad342@gmail.com>
2026-06-03 13:50:13 +0700

    update product toppings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,43 +33,43 @@ class DatabaseSeeder extends Seeder
             'is_active' => true,
         ]);
 
-        // ShopSetting::query()->create([
-        //     'shop_name' => 'Kasir Cafe',
-        //     'address' => 'Jl. Contoh No. 1',
-        //     'receipt_footer' => 'Terima kasih!',
-        //     'tax_percent' => 0,
-        //     'service_charge_percent' => 0,
-        // ]);
+        ShopSetting::query()->create([
+            'shop_name' => 'Kasir Cafe',
+            'address' => 'Jl. Contoh No. 1',
+            'receipt_footer' => 'Terima kasih!',
+            'tax_percent' => 0,
+            'service_charge_percent' => 0,
+        ]);
 
-        // $coffee = Categories::query()->create(['name' => 'Coffee', 'is_active' => true]);
-        // $nonCoffee = Categories::query()->create(['name' => 'Non-Coffee', 'is_active' => true]);
+        $coffee = Categories::query()->create(['name' => 'Coffee', 'is_active' => true]);
+        $nonCoffee = Categories::query()->create(['name' => 'Non-Coffee', 'is_active' => true]);
 
-        // $extraShot = Toppings::query()->create(['name' => 'Extra Shot', 'price' => 5000]);
-        // $oatMilk = Toppings::query()->create(['name' => 'Oat Milk', 'price' => 8000]);
-        // $boba = Toppings::query()->create(['name' => 'Boba', 'price' => 6000]);
+        $extraShot = Toppings::query()->create(['name' => 'Extra Shot', 'price' => 5000]);
+        $oatMilk = Toppings::query()->create(['name' => 'Oat Milk', 'price' => 8000]);
+        $boba = Toppings::query()->create(['name' => 'Boba', 'price' => 6000]);
 
-        // $espresso = Product::query()->create([
-        //     'category_id' => $coffee->id,
-        //     'name' => 'Espresso',
-        //     'description' => 'Single shot espresso.',
-        //     'price' => 18000,
-        // ]);
-        // $espresso->toppings()->sync([$extraShot->id, $oatMilk->id]);
+        $espresso = Product::query()->create([
+            'category_id' => $coffee->id,
+            'name' => 'Espresso',
+            'description' => 'Single shot espresso.',
+            'price' => 18000,
+        ]);
+        $espresso->toppings()->sync([$extraShot->id, $oatMilk->id]);
 
-        // $latte = Product::query()->create([
-        //     'category_id' => $coffee->id,
-        //     'name' => 'Cafe Latte',
-        //     'description' => 'Espresso with steamed milk.',
-        //     'price' => 28000,
-        // ]);
-        // $latte->toppings()->sync([$extraShot->id, $oatMilk->id]);
+        $latte = Product::query()->create([
+            'category_id' => $coffee->id,
+            'name' => 'Cafe Latte',
+            'description' => 'Espresso with steamed milk.',
+            'price' => 28000,
+        ]);
+        $latte->toppings()->sync([$extraShot->id, $oatMilk->id]);
 
-        // $matcha = Product::query()->create([
-        //     'category_id' => $nonCoffee->id,
-        //     'name' => 'Matcha Latte',
-        //     'description' => 'Premium matcha with milk.',
-        //     'price' => 30000,
-        // ]);
-        // $matcha->toppings()->sync([$oatMilk->id, $boba->id]);
+        $matcha = Product::query()->create([
+            'category_id' => $nonCoffee->id,
+            'name' => 'Matcha Latte',
+            'description' => 'Premium matcha with milk.',
+            'price' => 30000,
+        ]);
+        $matcha->toppings()->sync([$oatMilk->id, $boba->id]);
     }
 }
