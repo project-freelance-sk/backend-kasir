0a9ada5888661a6b7b1aedf762cda01a2f23f944
Nabil Bahroin <127678414+nb-b@users.noreply.github.com>
2026-06-09 13:49:10 +0700

    Fix syntax error in api.php routing

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test-cicd', function () {
     return "sukses";
-})
+});
 
 Route::prefix('auth')->group(function () {
     $loginMiddleware = app()->environment('testing') ? [] : ['throttle:10,1'];
